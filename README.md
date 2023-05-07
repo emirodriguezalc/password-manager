d2487654843724cb89f48726b7db7a09b3f87b12
Emilia-Rodriguez-Gimenez <e.gimenezrodriguez@doveconviene.com>
2023-05-07 13:13:06 +0200

    Add extra field to program

---
+++ README.md
@@ -0,0 +1,17 @@
+# Week 8: Dictionary-Based Password Manager
+
+You are required to create a dictionary-based password manager using Python programming language. The password manager should be able to store and manage user credentials in a secure manner. It should have a graphical user interface (GUI) created using a GUI library such as Tkinter or PyQt.
+
+## Features
+The password manager should have the following features:
+- Allow users to create a new account by entering their username and password.
+- Allow users to log in to their account using their username and password.
+- Allow users to store their website name, website URL, username, and password for each account.
+- Allow users to view, edit, and delete their stored account information.
+- Implement basic security features such as password strength checking, password hashing, and encryption of stored passwords.
+
+## Documentation
+The password manager should also be well-documented with clear comments and instructions.
+
+## Submission Guidelines
+- Submit your Python code along with a document that includes a screenshot of the GUI, a brief description of the password manager's features, and instructions on how to use it.
--- main.py
+++ main.py
@@ -41,17 +41,19 @@ def create_password():
 
 def save():
     email = email_entry.get()
-    website = website_entry.get()
+    website_name = website_name_entry.get()
+    website_url = website_url_entry.get()
     password = password_entry.get()
-    if len(website) == 0 or len(password) == 0:
+    if len(website_url) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Please fill all fields")
     else:
         is_ok = messagebox.askokcancel(
-            title=website, message=f"You've entered\nEmail: {email}\nPassword: {password}\n"f"Are you sure you want to save?")
+            title=website_name, message=f"You've entered\nEmail: {email}\nPassword: {password}\n"f"Are you sure you want to save?")
         if is_ok:
             data_to_save = {
                 "email": email,
-                "website": website,
+                "website_name": website_name,
+                "website_url": website_url,
                 "password": password,
             }
             file_name = 'passwords_data.json'
@@ -63,7 +65,8 @@ def save():
             data.append(data_to_save)
             with open(file_name, 'w') as f:
                 json.dump(data, f)
-                website_entry.delete(0, END)
+                website_name_entry.delete(0, END)
+                website_url_entry.delete(0, END)
                 password_entry.delete(0, END)
 
 # ---------- GUI ---------- #
@@ -82,31 +85,38 @@ canvas = Canvas(width=400, height=400)
 photo = PhotoImage(file='wolf.png')
 canvas.create_image(100, 100, image=photo)
 canvas.grid(column=1, row=0)
+# Allow users to store their website name, website URL, username, and password for each account.
 
 # Labels
-website = Label(text="Website: ")
-website.grid(column=0, row=1)
+website_name = Label(text="Website name: ")
+website_name.grid(column=0, row=1)
+
+website_url = Label(text="Website URL: ")
+website_url.grid(column=0, row=2)
 
 email = Label(text="Email/Username: ")
-email.grid(column=0, row=2)
+email.grid(column=0, row=3)
 
 password = Label(text="Password")
-password.grid(column=0, row=3)
+password.grid(column=0, row=4)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_name_entry = Entry(width=35)
+website_name_entry.grid(column=1, row=1, columnspan=2)
+
+website_url_entry = Entry(width=35)
+website_url_entry.grid(column=1, row=2, columnspan=2)
 email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.grid(column=1, row=3, columnspan=2)
 password_entry = Entry(width=20)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=4)
 
 # Buttons
 generate_button = Button(text="Generate Password", command=create_password)
-generate_button.grid(column=2, row=3)
+generate_button.grid(column=2, row=5)
 add_button = Button(text="Add", command=save)
-add_button.grid(column=1, row=5, columnspan=2)
+add_button.grid(column=1, row=6, columnspan=2)
 print_button = Button(text="Print saved", command=print_saved)
-print_button.grid(column=3, row=5)
+print_button.grid(column=3, row=7)
 
 screen.mainloop()
